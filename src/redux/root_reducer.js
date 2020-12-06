2786436ff405e58b7d7c71169df298171b521c3f
jeldikk <jeldi.kamal2013@gmail.com>
2020-12-06 13:47:01 +0530

    integrated redux-persist to avail localstorage browser API to persist group and users key information

--- src/redux/root_reducer.js
+++ src/redux/root_reducer.js
@@ -1,11 +1,21 @@
 import {combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import groupReducer from "./group/group.reducer"
 import userReducer from "./users/users.reducers"
 import editReducer from "./edit/edit.reducers"
 
-export default combineReducers({
+const persistConfig = {
+    key: 'user-group-root',
+    storage,
+    whitelist: ['group', 'users']
+}
+
+
+const rootReducer = combineReducers({
     group: groupReducer,
     users: userReducer,
     edit: editReducer
 })
+export default persistReducer(persistConfig, rootReducer)
